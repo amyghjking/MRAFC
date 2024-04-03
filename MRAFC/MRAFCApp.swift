4c7640c875cbc302f71d8e0a88b1d4fa4f351c54
amyghjking <amyking386@gmail.com>
2024-04-04 11:18:02 +1300

    Add Xcode project

---
+++ MRAFC/MRAFCApp.swift
@@ -0,0 +1,17 @@
+//
+//  MRAFCApp.swift
+//  MRAFC
+//
+//  Created by Amy King on 4/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MRAFCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
